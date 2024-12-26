4ca00572cd360a3b7979ae74d36674afb2280978
Andreas Drangel <andreas.drangel@gmail.com>
2024-12-26 22:38:15 +0100

    light white on connect

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ import 'babel-polyfill'
 import openSocket from 'socket.io-client'
 import SerialPort from 'serialport'
 import Delimiter from '@serialport/parser-delimiter'
+import { Gpio } from 'onoff'
 
-const SERVER_IP_ADDRESS = 'http://192.168.0.161:3001'
+const SERVER_IP_ADDRESS = 'http://192.168.0.85:3001'
 
 const SERIAL_PORT = '/dev/ttyUSB0'
+
+const RED_PIN = 17
+const GREEN_PIN = 27
+const BLUE_PIN = 22
+
+const redLight = new Gpio(RED_PIN, 'out')
+const greenLight = new Gpio(GREEN_PIN, 'out')
+const blueLight = new Gpio(BLUE_PIN, 'out')
+
+redLight.writeSync(0)
+greenLight.writeSync(0)
+blueLight.writeSync(0)
 
 const socket = openSocket(SERVER_IP_ADDRESS)
 
@@ -20,6 +33,10 @@ const parser = serial.pipe(new Delimiter({ delimiter: '\n' }))
 
 socket.on('connect', () => {
   console.log('connected')
+  redLight.writeSync(1)
+  greenLight.writeSync(1)
+  blueLight.writeSync(1)
+
   socket.emit('connect_rover')
 
   parser.on('data', data => {
@@ -44,5 +61,17 @@ socket.on('command', ({ type, command }) => {
     serial.write(JSON.stringify(command), 'ascii', error => { console.log('RESPONSE', error) })
   } else if (type === 'kill') {
     serial.write(JSON.stringify({ A: 0, B: 0 }), 'ascii')
+  } else if (type ==='color') {
+    switch (color) {
+      case 'red':
+        redLight.writeSync(1)
+        break
+      case 'green':
+        greenLight.writeSync(1)
+        break
+      case 'blue':
+        blueLight.writeSync(1)
+        break
+    }
   }
 })
